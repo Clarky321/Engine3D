76db00dc412fbd400ea88f5ddff44313557bf9df
LORAN <vinana222@gmail.com>
2024-01-20 16:07:21 +0300

    Added Engine3D, Engine3DCore, Engine3DEditor

---
+++ Engine3DCore/src/Engine3DCore/Utils/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "Engine3DCore/Utils/test.hpp"
+
+namespace Engine3D {
+
+	void sayHello()
+	{
+		std::cout << "Hello from Engine 3D Core" << std::endl;
+	}
+}
---
+++ Engine3DEditor/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include <Engine3DCore/Utils/test.hpp>
+
+int main()
+{
+	std::cout << "Hello from Engine 3D Editor" << std::endl;
+
+	Engine3D::sayHello();
+
+	std::cin.get();
+}
