f1a31aabd6a419d67f1285b1625e9c98c026626c
LORAN <vinana222@gmail.com>
2024-01-22 09:58:36 +0300

    added package library spdlog

--- Engine3DCore/src/Engine3DCore/Application.cpp
+++ Engine3DCore/src/Engine3DCore/Application.cpp
@@ -1,7 +1,5 @@
-#pragma once
-
 #include "Engine3DCore/Application.hpp"
-
+#include "Engine3DCore/Log.hpp"
 #include <GLFW/glfw3.h>
 
 #include <iostream>
@@ -10,7 +8,14 @@ namespace Engine3D {
 
     Application::Application()
     {
+        LOG_INFO("Welcome to spdlog!");
+        LOG_ERROR("Some error message with arg: {}", 1);
 
+        LOG_WARN("Easy padding in numbers like {:08d}", 12);
+        LOG_CRITICAL("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+        LOG_INFO("Support for floats {:03.2f}", 1.23456);
+        LOG_INFO("Positional args are {1} {0}..", "too", "supported");
+        LOG_INFO("{:<30}", "left aligned");
     }
 
     Application::~Application()
